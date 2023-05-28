b7328dea354609ccc634af7176666fe9f40f025e
Yusuf Iqbal <yusuf14t@gmail.com>
2023-05-28 16:36:38 +0530

    support

--- Client/src/components/common-components/Notification.js
+++ Client/src/components/common-components/Notification.js
@@ -9,7 +9,7 @@ const Notification = () => {
                     <button className="btn btn-light">+ Notification</button>
                 </div>
             </div> */}
-            <div className="row" >
+            <div className="row mr-0" >
                 <div class="col-xl-12 col-md-12">
                     <div class="ms-panel mb-0">
                         <div  class="ms-panel-header ms-panel-custome d-flex justify-space-between mb-2">
@@ -21,41 +21,41 @@ const Notification = () => {
                         </div>
                         </div>
                         <div class="ms-panel-body p-0" >
-                        <div class="table-responsive ">
-                            <div className="row ms-panel-header ">
-                                <div className="col">
+                        <div  class="table-responsive scrollbar-deep-purple ">
+                            <div style={{position:"absolute", width:"99%", zIndex:"9"}}  className="row mx-0 ms-panel-header ">
+                                <div className="col text-center ">
                                     <span className="text-light ml-4">People</span>
                                 </div>
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-light">Reciever</span>
                                 </div>                                
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-light">Message</span>
                                 </div>                                
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-light">Title</span>
                                 </div>                                
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-light">Status</span>
                                 </div>
                             </div>
-                            <div className="mx-4 mt-4">
+                            <div className="mx-4 mt-10  " style={{height:"65vh", marginTop:"50px"}}>
 
-                            {Array(15).fill(0).map( a =>
-                                 <div className="row dropdown-menu-active">
-                                <div className="col">
-                                    <span className="text-dark">Dr. Andrew </span>
+                            {Array(25).fill(0).map( a =>
+                                 <div className="row dropdown-menu-active ">
+                                <div className="col text-center ">
+                                    <span className="text-dark align-middle">Dr. Andrew </span>
                                 </div>
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-dark">01 Dec 2022</span>
                                 </div>                                
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-dark">Hello world</span>
                                 </div>                                
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-dark">Title</span>
                                 </div>                                
-                                <div className="col">
+                                <div className="col text-center">
                                     <span className="text-dark">
                                         <label class="ms-switch">
                                                 <input type="checkbox" />
@@ -64,65 +64,7 @@ const Notification = () => {
                                 </div>
                             </div>)}
                             </div>
-                            {/* <ul style={{overflowY:"scroll", height:"200px"}} className=" ">
-                                <ul  className="d-flex justify-content-around ms-panel-header position-sticky">
-                                <li>
-                                    <div>Title</div>
-                                </li>
-                                <li>
-                                    <div>Reciever</div>
-                                </li>
-                                <li>
-                                    <div>Date</div>
-                                </li>
-                                <li>
-                                    <div>Message</div>
-                                </li>
-                                <li>
-                                    <div>Status</div>
-                                </li>
-                                </ul>
-                                {[1,2,3,4,5,6,7].map( a => <li>
-                                        <td class="ms-table-f-w"> <img src="assets/img/dashboard/patient-6.jpg" alt="people" /> John Doe </td>
-                                        <td>Dr. Andrew </td>
-                                        <td>01 Dec 2022</td>
-                                        <td>Hello world</td>
-                                        <td>
-                                            <label class="ms-switch">
-                                                <input type="checkbox" />
-                                                <span class="ms-switch-slider ms-switch-success round"></span>
-                                            </label>
-                                        </td>
-                                    </li>)}
-                            </ul> */}
-                            {/* <table className="table table-hover thead-primary">
-                            <thead style={{position:"sticky"}}>
-                                <tr>
-                                <th scope="col">Title</th>
-                                <th scope="col">Reciever</th>
-                                <th scope="col">Date</th>
-                                <th scope="col">Message</th>
-                                <th scope="col">Status</th>
-                                </tr>
-                            </thead>
                             
-                                <tbody>
-                                    {[1,2,3,4,5,6,7].map( a => <tr>
-                                        <td class="ms-table-f-w"> <img src="assets/img/dashboard/patient-6.jpg" alt="people" /> John Doe </td>
-                                        <td>Dr. Andrew </td>
-                                        <td>01 Dec 2022</td>
-                                        <td>Hello world</td>
-                                        <td>
-                                            <label class="ms-switch">
-                                                <input type="checkbox" />
-                                                <span class="ms-switch-slider ms-switch-success round"></span>
-                                            </label>
-                                        </td>
-                                    </tr>)}
-                                </tbody>
-
-                            
-                            </table> */}
                         </div>
                         </div>
                     </div>
