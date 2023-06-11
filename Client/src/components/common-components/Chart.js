4e18ed572b6e7d2000a62f5732f7515cfdd00bd1
Hamson <realhamson@gmail.com>
2023-06-11 15:44:23 +0530

    sdf

---
+++ Client/src/components/common-components/Chart.js
@@ -0,0 +1,72 @@
+import { Line, Doughnut } from 'react-chartjs-2';
+import {  Chart as ChartJS, CategoryScale, LinearScale, PointElement,  LineElement, Title, Tooltip, Legend, ArcElement } from 'chart.js';
+
+ChartJS.register(
+CategoryScale,
+LinearScale,
+PointElement,
+LineElement,
+ArcElement,
+Title,
+Tooltip,
+Legend
+);
+
+
+const chartOptions = {
+    responsive: true,
+    plugins: {
+        legend: {
+            display: false,
+            position: 'top',
+        },
+        
+    },
+};
+
+
+export const LineChart = ({ filterType, labelName, chartData }) => {
+    const weekLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const monthLabels = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    const data = {
+        labels: filterType === 'week' ? weekLabels : monthLabels,
+        datasets: [
+            {
+                label: labelName,
+                data: filterType === 'week' ?  chartData.week || [] : chartData.month || [],
+                borderColor: '#112C2F',
+                backgroundColor: '#112C2F',
+                pointRadius: 3,
+                borderWidth: 2,
+            },
+        ],
+    };
+    return(
+        <Line options={chartOptions} data={data} />
+    )
+}
+
+
+export const DoughnutChart = () => {
+
+    const data = {
+        labels: ['Red', 'Blue', 'Yellow'],
+        datasets: [
+          {
+            label: 'Totals',
+            data: [2478,5267,734],
+            backgroundColor: ["#0B2447", "#8D72E1","#A5D7E8"],
+            borderColor: '#fff',
+            borderWidth: 3,
+          },
+        ],
+    }
+
+    return(
+        <Doughnut
+            options={chartOptions}
+            data={data}
+        />
+    )
+}
--- Client/src/components/user-component/mr/Dashboard.js
+++ Client/src/components/user-component/mr/Dashboard.js
@@ -1,11 +1,51 @@
-import React, {  useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import image from "../../../assets.app/img/dashboard/doctor-1.jpg"
 import { Link } from 'react-router-dom';
-import UserModal from '../../common-components/UserModal';
+import UserModal from '../../common-components/UserModal';  
+import { LineChart, DoughnutChart } from '../../common-components/Chart';
 
 const Dashbaord = () => {
     const [isUserModalOpen, setUserModalOpen] = useState(false)
     const [doctor, setDoctor] = useState({})
+    const [isWeekChart, setIsWeekChart] = useState(true) 
+    const [chartData, setChartData] = useState({})
+
+    useEffect(() => {
+        changeFilter('clinics')
+    }, [])
+    
+    const changeFilter = (value) => {
+        let data = {
+            week: [],
+            month: []
+        }
+        if(value === 'clinics') {
+            data = {
+                week:  ['2', '5', '1', '2', '3', '7', '9'],
+                month: ['3', '1', '6', '9', '3', '6', '8','3','4','2','6', '2']
+            }
+        }
+        else if( value === 'hospitals') {
+            data = {
+                week:  ['4', '3', '5', '3', '7', '8', '9'],
+                month: ['6', '3', '7', '8', '4', '2', '6','3','9','5','8', '4']
+            }
+        }
+        else if( value === 'visits') {
+            data = {
+                week:  ['1', '2', '3', '4', '3', '2', '3'],
+                month: ['1', '2', '3', '4', '3', '2', '3','3','7','4','8', '4']
+            }
+        }
+        else if( value === 'doctors') {
+            data = {
+                week:  ['6', '3', '6', '4', '8', '3', '6'],
+                month: ['5', '2', '4', '7', '8', '9', '6','5','4','2','4', '4']
+            }
+        }
+        setChartData(data)
+    } 
+
     return (
         <div className='ms-content-wrapper content-height'>
             <div class="ms-panel-header ms-panel-custome d-flex justify-space-between mb-2">
@@ -75,6 +115,64 @@ const Dashbaord = () => {
                             <i class="fas fa-stethoscope ms-icon-mr"></i>
                         </div>
                     </a>
+                </div>
+                <div class="col-xl-6 col-md-6 col-sm-12">
+                    <div class="ms-panel">
+                        <div class="ms-panel-header">
+                            <div className='d-flex justify-content-between'>
+                                <div>
+                                    <h6>Total Doctors</h6>
+                                </div>
+                                <div>
+                                    <div className='form-ontrol'>
+                                        <select class="form-control" 
+                                            onChange={(e) => changeFilter(e.target.value)}
+                                        >
+                                            <option value="clinics">Clinics</option>
+                                            <option value="doctors">Doctors</option>
+                                            <option value="hospitals">Hospitals</option>
+                                            <option value="visits">Visits</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            
+                        </div>
+                        <div class="ms-panel-body">
+                             <span className='h6'>Week</span>
+                            <label class="ms-switch mx-2">
+                                <input type="checkbox"
+                                    onChange={(e) => setIsWeekChart(!e.target.checked)}
+                                />
+                                <span class="ms-switch-slider ms-switch-dark round"></span>
+                            </label>
+                            <span className='h6'>Year</span>
+                            <LineChart filterType={isWeekChart ? 'week' : 'month' } labelName={'Patient'} chartData={chartData} />
+                        </div>
+                        
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-6 col-sm-12">
+                    <div class="ms-panel h-100">
+                        <div class="ms-panel-header">
+                            <div>
+                                <h6>Totals</h6>
+                            </div>
+                        </div>
+                        <div class="ms-panel-body d-flex justify-content-around align-items-center">
+                            <div>
+                                <span className='h4'>Organization</span>
+                                <div className='mt-3 float-left'>
+                                    <p className='h6'>Hospitals <span className='h5 text-info'>46%</span></p>
+                                    <p className='h6'>Clinics <span className='h5 text-info'>46%</span></p>
+                                    <p className='h6'>Doctors <span className='h5 text-info'>46%</span></p>
+                                </div>
+                            </div>
+                            <div className='text-center' style={{ height:'12rem', width:'12rem' }}>
+                                <DoughnutChart filterType={isWeekChart ? 'week' : 'month' } labelName={'Patient'} chartData={chartData} />
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div class="col-xl-6 col-md-6">
                     <div class="ms-panel ms-panel-fh ms-widget">
