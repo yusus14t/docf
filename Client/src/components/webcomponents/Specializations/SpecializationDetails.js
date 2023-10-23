2715f41d6a0e4f7e7b57eccc69a07e3efbad5740
Yusuf Iqbal <yusuf14t@gmail.com>
2023-10-23 23:25:13 +0530

    ll

--- Client/src/components/webcomponents/Specializations/SpecializationDetails.js
+++ Client/src/components/webcomponents/Specializations/SpecializationDetails.js
@@ -2,10 +2,12 @@ import React, { useEffect, useState } from "react";
 import "../../../assets.app/css/specialzation.css";
 import { Link, useParams } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAlignJustify, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
 import { getAuthHeader, getFullPath,axiosInstance, convertTo12HourFormat,  } from "../../../constants/utils";
 import clinicPhoto2 from "../../../assets.web/img/home-1/1920x1280-1.jpg";
 import { NUMBER_TO_DAY } from "../../../constants/constant";
+import NO_PHOTO from "../../../assets.app/images/no_images/no_clinic.jpg";
+
 
 
 const SpecializationDetails = () => {
@@ -163,15 +165,83 @@ const SpecializationDetails = () => {
             </div>
           </div>
           <div className="">
-            <h2 className="text-center">clincs</h2>
+            <h2 className="text-center">Clinics</h2>
             <div className="row">
               {clinics.length > 0 &&
                 clinics
                   .filter((e, i) => i < 3)
                   .map((clinic, key) => {
                     return (
+                      // <div
+                      //   className="col-lg-4 mb-4 col-md-4 mcard mt-4"
+                      //   key={key}
+                      // >
+                      //   <div
+                      //     style={{ background: "#ffffff" }}
+                      //     className="clinic-card "
+                      //   >
+                      //     <div className="inner-card-border"></div>
+                      //     <div style={{ marginBottom: "-22px" }}>
+                      //       <img
+                      //         className="clinic-img"
+                      //         src={
+                      //           clinic?.photo
+                      //             ? getFullPath(clinic?.photo)
+                      //             : clinicPhoto2
+                      //         }
+                      //         alt=""
+                      //       />
+                      //       <span className=" p-2 clinic-title">
+                      //         {clinic?.name}
+                      //       </span>
+                      //       <span style={{marginLeft:"10px", fontSize:"10px"}} className="ml-2 p-2 clinic-title">
+                      //         &#8377;{clinic?.fee}
+                      //       </span>
+                      //     </div>
+                      //     <div className="clinic-details d-flex flex-row justify-content-between">
+                      //       <div className="mt-2">
+                      //         <h6 className="clinic-specialization text-disabled">
+                      //         {clinic?.specialization.length ? clinic.specialization[0].name : "-"}
+                      //         </h6>
+                      //         <div className="contact-info">
+                      //           <h6 className="text-disabled mt-1">
+                      //             Contact Info :
+                      //           </h6>
+                      //           <div>
+                      //             <p className="ml-2 adjust hospital-address  ">
+                      //               <FontAwesomeIcon
+                      //                 className="clinic-icon address-icon"
+                      //                 icon={faLocationDot}
+                      //               />
+                      //               {clinic?.address || "-"}
+                      //             </p>
+                      //           </div>
+                      //         </div>
+                      //       </div>
+                      //       <div className="">
+                      //         <h6 className="text-disabled">Timming</h6>
+                      //         <div className="d-flex flex-column justify-contant-between">
+                      //           <div className="">
+                      //             <p className="clinic-timming mb-0">
+                      //               Morning : 08 AM to 11 PM
+                      //             </p>
+                      //             <p className="clinic-timming mb-0">
+                      //               Evening : 05 PM to 11 PM
+                      //             </p>
+                      //           </div>
+                      //           <Link
+                      //             className="text-light clinic-btn  btn btn1 btn-primary shadow-none"
+                      //             to={`/clinic-detail/${clinic?._id}`}
+                      //           >
+                      //             View More
+                      //           </Link>
+                      //         </div>
+                      //       </div>
+                      //     </div>
+                      //   </div>
+                      // </div>
                       <div
-                        className="col-lg-4 mb-4 col-md-4 mcard mt-4"
+                        className="col-lg-4 mb-4 col-md-6 mcard mt-4"
                         key={key}
                       >
                         <div
@@ -185,48 +255,55 @@ const SpecializationDetails = () => {
                               src={
                                 clinic?.photo
                                   ? getFullPath(clinic?.photo)
-                                  : clinicPhoto2
+                                  : NO_PHOTO
                               }
                               alt=""
                             />
                             <span className=" p-2 clinic-title">
                               {clinic?.name}
                             </span>
-                            <span style={{marginLeft:"10px", fontSize:"10px"}} className="ml-2 p-2 clinic-title">
+                            <span
+                              style={{ marginLeft: "10px", fontSize: "15px" }}
+                              className="ml-2 p-2 clinic-title"
+                            >
                               &#8377;{clinic?.fee}
                             </span>
                           </div>
                           <div className="clinic-details d-flex flex-row justify-content-between">
                             <div className="mt-2">
                               <h6 className="clinic-specialization text-disabled">
-                              {clinic?.specialization.length ? clinic.specialization[0].name : "-"}
+                                {clinic.specialization.length > 1
+                                  ? "Multi speciality"
+                                  : clinic.specialization[0]?.name || "-"}
                               </h6>
                               <div className="contact-info">
                                 <h6 className="text-disabled mt-1">
                                   Contact Info :
                                 </h6>
                                 <div>
+                                  <FontAwesomeIcon
+                                    className="clinic-icon"
+                                    icon={faPhone}
+                                  />
+                                  <p className="d-inline-block ml-2 mb-0">
+                                    {clinic?.phone ? "+91" + clinic?.phone : ""}
+                                  </p>
+                                </div>
+                                <div>
                                   <p className="ml-2 adjust hospital-address  ">
                                     <FontAwesomeIcon
                                       className="clinic-icon address-icon"
                                       icon={faLocationDot}
                                     />
-                                    {clinic?.address || "-"}
+                                    {clinic?.address}
                                   </p>
                                 </div>
                               </div>
                             </div>
                             <div className="">
                               <h6 className="text-disabled">Timming</h6>
-                              <div className="d-flex flex-column justify-contant-between">
-                                <div className="">
-                                  <p className="clinic-timming mb-0">
-                                    Morning : 08 AM to 11 PM
-                                  </p>
-                                  <p className="clinic-timming mb-0">
-                                    Evening : 05 PM to 11 PM
-                                  </p>
-                                </div>
+                              <div className="d-flex flex-column justify-contant-around">
+                                {getTodayTiming(clinic?.timing)}
                                 <Link
                                   className="text-light clinic-btn  btn btn1 btn-primary shadow-none"
                                   to={`/clinic-detail/${clinic?._id}`}
